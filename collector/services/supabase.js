f8ccd3471e2c5821a9063882736da06067ad2be5
Shiva Prasad <Shivaprasadgunaganti77@gmail.com>
2026-06-20 12:49:18 +0530

    fix gitgub websocket issue

--- collector/services/supabase.js
+++ collector/services/supabase.js
@@ -1,10 +1,27 @@
+// require("dotenv").config();
+
+// const { createClient } = require("@supabase/supabase-js");
+
+// const supabase = createClient(
+//   process.env.SUPABASE_URL,
+//   process.env.SUPABASE_SERVICE_ROLE_KEY
+// );
+
+// module.exports = supabase;
+
 require("dotenv").config();
 
 const { createClient } = require("@supabase/supabase-js");
+const WebSocket = require("ws");
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY,
+  {
+    realtime: {
+      transport: WebSocket,
+    },
+  }
 );
 
 module.exports = supabase;
